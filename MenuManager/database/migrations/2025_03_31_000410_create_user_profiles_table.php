fca04a7ffb6731cf0a01c7d3e389a95567b8dbad
victoracad <victoracad123@gmail.com>
2025-03-30 21:08:45 -0300

    Criando o banco

--- MenuManager/database/migrations/2025_03_31_000410_create_user_profiles_table.php
+++ MenuManager/database/migrations/2025_03_31_000410_create_user_profiles_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('admin_type');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('avatar_image');
+            $table->foreignId('users_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_profiles');
     }
 };
--- MenuManager/database/migrations/2025_03_31_000827_create_dishes_table.php
+++ MenuManager/database/migrations/2025_03_31_000827_create_dishes_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('dishes', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('admin_type');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('dishes');
     }
 };
